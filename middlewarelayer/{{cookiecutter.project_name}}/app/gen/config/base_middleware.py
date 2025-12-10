05012098d3328b52565188488ac139a3da56a09e
michael from semaverse dev <mail@semaverse.com>
2025-12-10 20:59:10 +0100

    add broker injection

---
+++ middlewarelayer/{{cookiecutter.project_name}}/app/gen/config/base_middleware.py
@@ -0,0 +1,20 @@
+from app.gen.config.base import BaseEnvironmentContext
+from app.gen.middleware.AgentBroker import AgentBroker
+
+
+class BaseMiddlewareEnvironmentContext():
+
+    baseEnvironment = BaseEnvironmentContext()
+
+    def __init__(self):
+    {%- for key, agent in cookiecutter.agents.items() %}
+        self.{{agent.uid | aiurnvar | capitalize }}AgentBrokerBean()
+
+    {%- for key, agent in cookiecutter.agents.items() %}
+    def {{agent.uid | aiurnvar | capitalize }}AgentBrokerBean(self):
+        agent = baseEnvironment.{{agent.uid | aiurnvar | capitalize }}AgentBean()
+        return AgentBroker(agent)
+
+    {%- endfor %}
+
+
